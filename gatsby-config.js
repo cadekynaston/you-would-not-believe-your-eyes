8edb54681a24870176553d69469f23785d209b3f
Cade Kynaston <cade.kynaston@clearlink.com>
2019-06-15 12:12:06 -0600

    added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-137604730-3",
+      },
+    },
   ],
 }
