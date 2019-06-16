901d2f60d14cf33ade33a472956f89956f2912b2
Cade Kynaston <cade.kynaston@clearlink.com>
2019-06-16 14:54:38 -0600

    Update Video.js

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export const Video = () => (
-  <iframe className="video-iframe" src="https://www.youtube.com/embed/XtlzvsV0P7M?autoplay=1&loop=1&color=white" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+  <iframe className="video-iframe" src="https://www.youtube.com/embed/XtlzvsV0P7M?color=white" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
 )
